aae5ac0119fa332a7a0a3d86203e169cc2c6ff8a
epoundor <freedausstanda@gmail.com>
2023-05-22 21:41:55 +0100

    deploy

---
+++ src/views/BlogPage2.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-white text-center py-10">
+        <h3 class="font-bold text-2xl uppercase">{{type}}</h3>
+        <div class="container">
+            <PostList :type="type"/>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import PostList from '../components/PostList.vue';
+defineProps<{ type: string }>()
+
+</script>
